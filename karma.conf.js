1e9791979687c8ef8c2b9bc17ec6d3ffeb575e6f
Javier Batres <fjbatresv@gmail.com>
2025-12-09 22:28:42 -0600

    :sparkles: Integrate sonarcloud for local and ci

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,11 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/javier-batres-site'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
+      ]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9877,
