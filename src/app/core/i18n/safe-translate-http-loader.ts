43f9c59ca7c5180812fec7bc35524cb0602c93bb
Javier Batres <fjbatresv@gmail.com>
2025-12-09 22:05:44 -0600

    Tech/upg deps 20251209 (#20)
    * :arrow_up: Applied upgrades requested by dependabot
    
    * :arrow_up: Engine set to node 24
    
    * :memo: Added sonarcloud quality gate badge
    
    * :memo: Added sonarcloud coverage batch to README
    
    * :sparkles: Added coderabbit.yml

--- src/app/core/i18n/safe-translate-http-loader.ts
+++ src/app/core/i18n/safe-translate-http-loader.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslationObject } from '@ngx-translate/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import * as Sentry from '@sentry/browser';
@@ -39,15 +39,15 @@ export class SafeTranslateHttpLoader implements TranslateLoader {
     private readonly suffix = '.json'
   ) {}
 
-  getTranslation(lang: string): Observable<Record<string, unknown>> {
-    return this.http.get<Record<string, unknown>>(`${this.prefix}${lang}${this.suffix}`).pipe(
+  getTranslation(lang: string): Observable<TranslationObject> {
+    return this.http.get<TranslationObject>(`${this.prefix}${lang}${this.suffix}`).pipe(
       catchError((error) => {
         reportTranslationLoadFailure(lang, error);
         console.warn(
           `Falling back to empty translations because ${lang} could not be loaded.`,
           error
         );
-        return of({});
+        return of({} as TranslationObject);
       })
     );
   }
